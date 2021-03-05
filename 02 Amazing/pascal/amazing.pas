fb26d9d101516ac61b66e66692f7f47fb4b5aaf7
Gustavo Carreno <guscarreno@gmail.com>
2021-03-05 15:04:08 +0000

    feat: Complete pascal implementation of Amazing

--- 02 Amazing/pascal/amazing.pas
+++ 02 Amazing/pascal/amazing.pas
@@ -7,11 +7,20 @@ program Amazing;
 uses
   Crt;
 
+type
+  TDirection = (dUp, dRight, dDown, dLeft);
+  TDirections = set of TDirection;
+
 var
   Width: Integer;   // H
-  Height: Integer;  // V
-  MatrixW: Array of Array of Integer;
-  MatrixV: Array of Array of Integer;
+  Length: Integer;  // V
+  Entry: Integer;
+  MatrixWalls: Array of Array of Integer;
+  MatrixVisited: Array of Array of Integer;
+
+const
+  EXIT_DOWN  = 1;
+  EXIT_RIGHT = 2;
 
 procedure PrintGreeting;
 begin
@@ -26,36 +35,201 @@ end;
 procedure GetDimensions;
 begin
   repeat
-    Write('WHAT ARE YOUR WIDTH AND LENGTH ');
-    ReadLN(Width, Height);
-    if (Width = 1) or (Height = 1) then
+    Write('WHAT ARE YOUR WIDTH AND LENGTH (SPACE IN BETWEEN): ');
+    ReadLN(Width, Length);
+    if (Width = 1) or (Length = 1) then
     begin
       WriteLN('MEANINGLESS DIMENSIONS.  TRY AGAIN.');
     end;
-  until (Width > 1) and (Height > 1);
+  until (Width > 1) and (Length > 1);
+  WriteLN;
+  WriteLN;
+  WriteLN;
+  WriteLN;
+end;
+
+procedure ClearMatrices;
+var
+  indexW: Integer;
+  indexL: Integer;
+begin
+  SetLength(MatrixWalls, Width, Length);
+  SetLength(MatrixVisited, Width, Length);
+  for indexW:= 0 to Pred(Width) do
+  begin
+    for indexL:= 0 to Pred(Length) do
+    begin
+      MatrixWalls[indexW][indexL]:= 0;
+      MatrixVisited[indexW][indexL]:= 0;
+    end;
+  end;
+end;
+
+function GetRandomDirection(const ADirections: TDirections): TDirection;
+var
+  count: Integer;
+  position: Integer;
+  directions: array [0..3] of TDirection;
+begin
+  count:= 0;
+  position:= 0;
+  if dUp in ADirections then
+  begin
+    Inc(count);
+    directions[position]:= dUp;
+    Inc(position);
+  end;
+  if dRight in ADirections then
+  begin
+    Inc(count);
+    directions[position]:= dRight;
+    Inc(position);
+  end;
+  if dDown in ADirections then
+  begin
+    Inc(count);
+    directions[position]:= dDown;
+    Inc(position);
+  end;
+  if dLeft in ADirections then
+  begin
+    Inc(count);
+    directions[position]:= dLeft;
+    Inc(position);
+  end;
+  Result:= directions[Random(count)];
+end;
+
+procedure BuildMaze;
+var
+  indexW: Integer;
+  indexL: Integer;
+  direction: TDirection;
+  directions: TDirections;
+  count: Integer;
+begin
+  Entry:= Random(Width);
+  indexW:= Entry;
+  indexL:= 0;
+  count:= 1;
+  MatrixVisited[indexW][indexL]:= count;
+  Inc(count);
+  repeat
+    directions:= [dUp, dRight, dDown, dLeft];
+    if (indexW = 0) or (MatrixVisited[Pred(indexW)][indexL] <> 0) then
+    begin
+      Exclude(directions, dLeft);
+    end;
+    if (indexL = 0) or (MatrixVisited[indexW][Pred(indexL)] <> 0) then
+    begin
+      Exclude(directions, dUp);
+    end;
+    if (indexW = Pred(Width)) or (MatrixVisited[Succ(indexW)][indexL] <> 0) then
+    begin
+      Exclude(directions, dRight);
+    end;
+    if (indexL = Pred(Length)) or (MatrixVisited[indexW][Succ(indexL)] <> 0) then
+    begin
+      Exclude(directions, dDown);
+    end;
+
+    if directions <> [] then
+    begin
+      direction:= GetRandomDirection(directions);
+      case direction of
+        dLeft:begin
+          Dec(indexW);
+          MatrixWalls[indexW][indexL]:= EXIT_RIGHT;
+        end;
+        dUp:begin
+          Dec(indexL);
+          MatrixWalls[indexW][indexL]:= EXIT_DOWN;
+        end;
+        dRight:begin
+          Inc(MatrixWalls[indexW][indexL], EXIT_RIGHT);
+          Inc(indexW);
+        end;
+        dDown:begin
+          Inc(MatrixWalls[indexW][indexL], EXIT_DOWN);
+          Inc(indexL);
+        end;
+      end;
+      MatrixVisited[indexW][indexL]:= count;
+      Inc(count);
+    end
+    else
+    begin
+      while True do
+      begin
+        if indexW <> Pred(Width) then
+        begin
+          Inc(indexW);
+        end
+        else if indexL <> Pred(Length) then
+        begin
+          Inc(indexL);
+          indexW:= 0;
+        end
+        else
+        begin
+          indexW:= 0;
+          indexL:= 0;
+        end;
+        if MatrixVisited[indexW][indexL] <> 0 then
+        begin
+          break;
+        end;
+      end;
+    end;
+  until count = (Width * Length) + 1;
+  indexW:= Random(Width);
+  indexL:= Pred(Length);
+  Inc(MatrixWalls[indexW][indexL]);
+end;
+
+procedure DegubVisited;
+var
+  indexW: Integer;
+  indexL: Integer;
+begin
+  WriteLN('Visited');
+  for indexL:= 0 to Pred(Length) do
+  begin
+    for indexW:= 0 to Pred(Width) do
+    begin
+      Write(MatrixVisited[indexW][indexL]:2,' ');
+    end;
+    WriteLN;
+  end;
+  WriteLN;
+end;
+
+procedure DebugWalls;
+var
+  indexW: Integer;
+  indexL: Integer;
+begin
+  WriteLN('Walls');
+  for indexL:= 0 to Pred(Length) do
+  begin
+    for indexW:= 0 to Pred(Width) do
+    begin
+      Write(MatrixWalls[indexW, indexL]:2, ' ');
+    end;
+    WriteLN;
+  end;
+  WriteLN;
 end;
 
 procedure PrintMaze;
 var
-  Q: Integer;
-  Z: Integer;
-  X: Integer;
-  C: Integer;
-  R: Integer;
-  S: Integer;
-  I: Integer;
-  J: Integer;
+  indexW: Integer;
+  indexL: Integer;
 begin
-  SetLength(MatrixW, Width, Height);
-  SetLength(MatrixV, Width, Height);
-  Q:= 0;
-  Z:= 0;
-  X:= Random(Width) + 1;
 
-  // Top wall with entry on X
-  for I:= 0 to Pred(Width) do
+  for indexW:= 0 to Pred(Width) do
   begin
-    if Succ(I) = X then
+    if indexW = Entry then
     begin
       Write('.  ');
     end
@@ -64,64 +238,13 @@ begin
       Write('.--');
     end;
   end;
-  WriteLn('.');
-
-  C:= 1;
-  MatrixW[Pred(X), Pred(1)]:= C;
-  Inc(C);
-
-  R:= X;
-  S:= 1;
-
-  {while True do
-  begin
-    // THIS IS INSIDE LOOP OF SORTS
-    if Pred(R) = 0 then  //                        260 -> 530
-    begin
-      if Pred(S) = 0 then //                       530 -> 670
-      begin
-        if R = Width then //                       670 -> 740
-        begin
-          if S <> Height then //                   740 -> 760
-          begin
-            if MatrixW[Pred(R), Pred(S + 1)] <> 0 then //     760 -> 780
-            begin
-              continue;
-            end
-            else
-            begin
-              //  770 -> 910
-              if Q = 1 then //  910 -> 960
-              begin
-                Z:= 1;
-                if MatrixV[Pred(R), Pred(S)] = 0 then // 970 -> 980
-                begin
-                  MatrixV[Pred(R), Pred(S)]:= 1;
-                  Q:= 0;
-                  R:= 1;
-                  S:= 1;// GOTO 250
-                end
-                else
-                begin
-                  MatrixV[Pred(R), Pred(S)]:= 3;
-                  Q:= 0;
-                  continue;
-                end;
-              end;
-            end;
-          end;
-        end;
-      end;
-    end;
-    // THIS IS INSIDE LOOP OF SORTS
-  end;}
-
-  for J:= 0 to Pred(Height) do
+  WriteLN('.');
+  for indexL:= 0 to Pred(Length) do
   begin
     Write('I');
-    for I:= 0 to Pred(Width) do
+    for indexW:= 0 to Pred(Width) do
     begin
-      if MatrixV[I, J] < 2 then
+      if MatrixWalls[indexW, indexL] < 2 then
       begin
         Write('  I');
       end
@@ -131,26 +254,31 @@ begin
       end;
     end;
     WriteLN;
-    for I:= 0 to Pred(Width) do
+    for indexW:= 0 to Pred(Width) do
     begin
-      if (MatrixV[I, J] = 0) or (MatrixV[I, J] = 2) then
+      if (MatrixWalls[indexW, indexL] = 0) or (MatrixWalls[indexW, indexL] = 2) then
       begin
-        Write(':  ');
+        Write(':--');
       end
       else
       begin
-        Write(':--');
+        Write(':  ');
       end;
     end;
     WriteLN('.');
   end;
+  WriteLN;
 end;
 
 begin
   Randomize;
-  //ClrScr;
+  ClrScr;
   PrintGreeting;
   GetDimensions;
+  ClearMatrices;
+  BuildMaze;
+  //DegubVisited;
+  //DebugWalls;
   PrintMaze;
 end.
 
